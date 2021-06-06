77ec785cd6b46340420b9b6c34a3601e8461c81d
DerrickDarku <derrickalberto@ymail.com>
2021-06-06 13:12:55 +0100

    Navbar created

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <h1>HWAN KREATIVEZ</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components'; 
+
+const Nav = styled.nav`
+height:60px;
+background: blue;
+
+`;
+export default function Navbar() {
+    return (
+        <Nav>
+            <h1>HWANWORLD</h1>
+        </Nav>
+    )
+}
