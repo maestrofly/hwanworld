a74cca97bf2c60cd67bf7330d461010d40b7ca8a
DerrickDarku <derrickalberto@ymail.com>
2021-06-07 10:19:10 +0100

    Navbar created

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,78 @@
 import React from 'react';
-import styled from 'styled-components'; 
+import styled, { css } from 'styled-components/macro'; 
+import { Link } from 'react-router-dom';
+import { menuData } from '../components/data/MenuData';
+import { Button } from './Button';
 
 const Nav = styled.nav`
-height:60px;
+height: 60px;
+display: flex;
+justify-content: space-between;
+padding: 1rem 2rem;
+z-index: 100;
+position: fixed;
+width: 100%;
 background: blue;
+`;
+
+const NavLink =css`
+ color: #fff;
+ display: flex;
+ align-items: center;
+ padding: 0 1rem;
+ height: 100%;
+ cursor: pointer;
+ text-decoration: none;
+`
+
+
+const Logo = styled(Link)`
+ ${NavLink}
+ font-style: italic;
+`;
+
+const MenuBars = styled.i`
+`;
+
+const NavMenu = styled.div`
+ display: flex;
+ align-items: center;
+`;
+
+const NavMenuLinks = styled(Link)`
+${NavLink}
+`;
+
+const NavBtn = styled.div`
+display: flex;
+align-items: center;
+margin-right: 24px;
 
 `;
-export default function Navbar() {
+
+
+
+const Navbar = () => {
     return (
+        
         <Nav>
-            <h1>HWANWORLD</h1>
+            <Logo to='/'>HWAN</Logo>
+            <MenuBars />
+            <NavMenu>
+                {menuData.map((item, index) => (
+                    <NavMenuLinks to={item.link} key={index}>
+                        {item.title}
+                    </NavMenuLinks>
+    ))}
+            </NavMenu>
+            <NavBtn>
+                <Button to='/contact' primary='true'>
+                    Contact Us
+                </Button>
+            </NavBtn>
+            
         </Nav>
     )
-}
+};
+
+export default Navbar;
---
+++ src/components/data/MenuData.js
@@ -0,0 +1,5 @@
+export const menuData = [
+    { title: 'Events Management', link: '/eventsmanagement'},
+    { title: 'Blog', link: '/blog'},
+    { title: 'Hwan Foundation', link: '/hwanfoundation'},
+]
