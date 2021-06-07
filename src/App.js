c3de9b93fa3204554b3fb63bfe8002ad32b6af57
DerrickDarku <derrickalberto@ymail.com>
2021-06-07 17:45:59 +0100

    hero image slider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import GlobalStyle from './globalStyles';
+import Hero from './components/Hero';
+import { SliderData } from './components/data/SliderData';
 
 function App() {
   return (
-    <div>
+    <>
+      <GlobalStyle />
       <Navbar />
-      <h1>HWAN KREATIVEZ</h1>
-    </div>
+      <Hero slides={SliderData} />
+    </>
   );
 }
 
--- src/components/Button.js
+++ src/components/Button.js
@@ -22,6 +22,6 @@ export const Button = styled(Link)`
 font-size: ${({ big }) => (big ? '20px' : '14px')};
 
 &:hover {
-    transform: translateY() (-2px);
+    transform: translateY(-2px);
 }
 `;
